faa5bf9fe61aff2ba1459caed5e92a2b4b2b3ab6
sr-popug <antonpower2008@gmail.com>
2024-11-30 16:03:52 +0300

    Deploy on Next JS (without highlight-js)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import Footer from '@/components/common/Footer/Footer'
 import Header from '@/components/common/Header/Header'
 import MiniModal from '@/components/common/MiniModal/MiniModal'
-import { H } from 'highlight.run'
 import type { Metadata } from 'next'
 import './globals.scss'
+
 // const geistMono = localFont({
 //   src: "./fonts/GeistMonoVF.woff",
 //   variable: "--font-geist-mono",
@@ -14,20 +14,7 @@ export const metadata: Metadata = {
   title: 'Neduca',
   description: 'Платформа тренировок',
 }
-H.init('<YOUR_PROJECT_ID>', {
-  serviceName: 'frontend-app',
-  tracingOrigins: true,
-  networkRecording: {
-    enabled: true,
-    recordHeadersAndBody: true,
-    urlBlocklist: [
-      // insert full or partial urls that you don't want to record here
-      // Out of the box, Highlight will not record these URLs (they can be safely removed):
-      'https://www.googleapis.com/identitytoolkit',
-      'https://securetoken.googleapis.com',
-    ],
-  },
-})
+
 export default function RootLayout({
   children,
 }: Readonly<{
